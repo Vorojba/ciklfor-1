b406e991fec4ff2192201421bce35536cca062ba
h <g>
2021-11-16 11:06:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            int A = 5, B = 13,k=0;
+            for (int i=A;i<=B;i++)
+            {
+                Console.WriteLine(" " + i.ToString());
+                k++;
+            }
+            Console.WriteLine(" Количество: " + k.ToString());
+            Console.ReadKey();
+        }
+    }
+}
